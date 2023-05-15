0614e6d71d1df7b1ba9e7c8090d52ba512cc68a8
semihbeyzade <semihb.orscelik0@gmail.com>
2023-05-15 19:39:17 +0200

    dataTable

--- src/components/ErrorLog.tsx
+++ src/components/ErrorLog.tsx
@@ -1,17 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { DataGrid, GridColDef, GridRowId } from '@mui/x-data-grid';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
 
 interface ErrorData {
-  idUser: number;
+  id: number;
   message: string;
   level: number;
   source: number;
   // Diğer özellikler
 }
 
-const ErrorLog: React.FC = () => {
-  const [errorData, setErrorData] = useState<GridRowId[]>([]);
+const ErrorLog = () => {
+  const [errorData, setErrorData] = useState<ErrorData[]>([]);
 
   useEffect(() => {
     fetchData();
@@ -22,26 +22,39 @@ const ErrorLog: React.FC = () => {
       const response = await axios.post<ErrorData[]>(
         'https://data.autohaus-digital.de/log/v1/search/-1/-1/-1/-1/-1/-1/-1/-1/-1/-1/-1'
       );
-        setErrorData(response.data);
-   /*   setErrorData(response.data.map((error) => ({ id: error.id, message: error.message, level: error.level, source: error.source }))); */
+      setErrorData(response.data);
     } catch (error) {
       console.error(error);
     }
   };
 
-  const columns: GridColDef[] = [
-    { field: 'id', headerName: 'ID', width: 100 },
-    { field: 'message', headerName: 'Error Message', width: 300 },
-    { field: 'level', headerName: 'Level', width: 150 },
-    { field: 'source', headerName: 'Source', width: 150 },
-    // Diğer sütunlar
-  ];
-
   return (
-    <div style={{ height: 400, width: '100%' }}>
-      <DataGrid rows={errorData} columns={columns} pagination />
-    </div>
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>ID</TableCell>
+            <TableCell>Error Message</TableCell>
+            <TableCell>Level</TableCell>
+            <TableCell>Source</TableCell>
+            {/* Diğer sütunlar */}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {errorData.map((error) => (
+            <TableRow key={error.id}>
+              <TableCell>{error.id}</TableCell>
+              <TableCell>{error.message}</TableCell>
+              <TableCell>{error.level}</TableCell>
+              <TableCell>{error.source}</TableCell>
+              {/* Diğer sütunlar */}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
+
 
 export default ErrorLog;
